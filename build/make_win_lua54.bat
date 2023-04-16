1d9f1eebf6a883788965cef30d6572878abc09df
蒋佳李 <1036226474@qq.com>
2023-04-16 08:34:13 +0800

    优化编译

--- build/make_win_lua54.bat
+++ build/make_win_lua54.bat
@@ -1,5 +1,5 @@
 mkdir build64_54 & pushd build64_54
-cmake -DLUA_VERSION=5.4.3 -G "Visual Studio 16 2019" -A x64  ..
+cmake -DLUA_VERSION=5.4.1 -G "Visual Studio 16 2019" -A x64  ..
 popd
 cmake --build build64_54 --config Release
 md plugin_lua54\Plugins\x86_64
