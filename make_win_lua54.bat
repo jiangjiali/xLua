5ca9b7943fc167e6b73532b8ed6eddc71196cf2f
蒋佳李 <jiali.jiang@ustargames.com>
2023-04-17 14:43:25 +0800

    2.1.16

---
+++ make_win_lua54.bat
@@ -0,0 +1,8 @@
+mkdir build64_54 & pushd build64_54
+cmake -DLUA_VERSION=5.4.3 -G "Visual Studio 16 2019" -A x64  ..
+popd
+cmake --build build64_54 --config Release
+md plugin_lua54\Plugins\x86_64
+copy /Y build64_54\Release\xlua.dll plugin_lua54\Plugins\x86_64\xlua.dll
+
+pause
