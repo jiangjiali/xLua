45778ce27fac751515bd58e9b17deeffd8f385aa
蒋佳李 <1036226474@qq.com>
2023-04-19 21:39:25 +0800

    优化

--- lua-framework/f_math.cpp
+++ lua-framework/f_math.cpp
@@ -1,10 +1,7 @@
 #include <f_math.h>
-#include "boost/lexical_cast.hpp"
 
 // 函数返回两个数中较大的那个数
 int math::add(int num1, int num2) {
     int sum = num1 + num2;
-    int a = boost::lexical_cast<int>("123");
-    printf("%d\n",a);
     return sum;
 }
