da7185335100a5ee37f33d393c8d43d4e87824d1
蒋佳李 <1036226474@qq.com>
2023-04-21 21:10:40 +0800

    优化编译

--- luac/make_win64.bat
+++ luac/make_win64.bat
@@ -2,8 +2,8 @@ mkdir build64 & pushd build64
 cmake -DLUAC_COMPATIBLE_FORMAT=ON -G "Visual Studio 17 2022" -A x64 ..
 popd
 cmake --build build64 --config Release
-mkdir plugin_lua54\Plugins\luac
-copy /Y build64\Release\luac.exe plugin_lua54\Plugins\luac\luac.exe
-copy /Y build64\Release\lua.exe plugin_lua54\Plugins\luac\lua.exe
+md plugin_lua53\Plugins\luac
+copy /Y build64\Release\luac.exe plugin_lua53\Plugins\luac\luac.exe
+copy /Y build64\Release\lua.exe plugin_lua53\Plugins\luac\lua.exe
 
 pause
