295ca85b42efcc123038c5c3fec6673f90dec6e1
蒋佳李 <jiali.jiang@ustargames.com>
2023-04-21 13:51:09 +0800

    优化编译

--- luac/make_win64.bat
+++ luac/make_win64.bat
@@ -2,7 +2,7 @@ mkdir build64 & pushd build64
 cmake -DLUAC_COMPATIBLE_FORMAT=ON -G "Visual Studio 16 2019" -A x64 ..
 popd
 cmake --build build64 --config Release
-md ./../plugin_lua54\Plugins\exe
-copy /Y build64\Release\luac.exe ./../plugin_lua54\Plugins\exe\luac.exe
+md plugin_lua54\Plugins\exe
+copy /Y build64\Release\luac.exe plugin_lua54\Plugins\exe\luac.exe
 
 pause
