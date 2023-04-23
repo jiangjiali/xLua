ff15961eff1c1f5ce734acc91100792510cb64f4
蒋佳李 <jiali.jiang@ustargames.com>
2023-04-23 13:36:50 +0800

    优化编译

--- make_ios_lua53.sh
+++ make_ios_lua53.sh
@@ -1,5 +1,6 @@
 mkdir -p build_ios && cd build_ios
-cmake -DPLATFORM=OS64 -DLUA_VERSION=5.3.5 -DLUAC_COMPATIBLE_FORMAT=ON -DCMAKE_BUILD_TYPE=Release
+cmake -DPLATFORM=OS64 -DLUA_VERSION=5.3.5 -DLUAC_COMPATIBLE_FORMAT=ON -DCMAKE_BUILD_TYPE=Release -GXcode ../
+cd ..
 cmake --build build_ios --config Release
 mkdir -p plugin_lua53/Plugins/iOS/
 cp build_ios/Release-iphoneos/libxlua.a plugin_lua53/Plugins/iOS/libxlua.a 
