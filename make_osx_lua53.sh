56474a602c1688c6856e80bf93875103a205c9f4
蒋佳李 <jiali.jiang@ustargames.com>
2023-04-23 12:49:19 +0800

    编译优化

--- make_osx_lua53.sh
+++ make_osx_lua53.sh
@@ -1,5 +1,6 @@
 mkdir -p build_osx && cd build_osx
-cmake -DLUA_VERSION=5.3.5 -DLUAC_COMPATIBLE_FORMAT=ON -DCMAKE_BUILD_TYPE=Release -GXcode /
+cmake -DLUA_VERSION=5.3.5 -DLUAC_COMPATIBLE_FORMAT=ON -DCMAKE_BUILD_TYPE=Release -GXcode ../
+cd ..
 cmake --build build_osx --config Release
 mkdir -p plugin_lua53/Plugins/xlua.bundle/Contents/MacOS/
 cp build_osx/Release/xlua.bundle/Contents/MacOS/xlua plugin_lua53/Plugins/xlua.bundle/Contents/MacOS/xlua
--- make_osx_silicon_lua53.sh
+++ make_osx_silicon_lua53.sh
@@ -1,5 +1,6 @@
 mkdir -p build_osx_silicon && cd build_osx_silicon
-cmake -DBUILD_SILICON=ON -DLUA_VERSION=5.3.5 -DLUAC_COMPATIBLE_FORMAT=ON -DCMAKE_BUILD_TYPE=Release -GXcode /
+cmake -DBUILD_SILICON=ON -DLUA_VERSION=5.3.5 -DLUAC_COMPATIBLE_FORMAT=ON -DCMAKE_BUILD_TYPE=Release -GXcode ../
+cd ..
 cmake --build build_osx_silicon --config Release
 mkdir -p plugin_lua53/Plugins/arm64/
 cp build_osx_silicon/Release/libxlua.dylib plugin_lua53/Plugins/arm64/
