ee2d8a478b3f7f97b0b3a2a7a9dcb41f94e29a9e
蒋佳李 <jiali.jiang@ustargames.com>
2023-04-23 12:30:28 +0800

    移除

--- make_win_lua53.bat
+++
@@ -1,8 +0,0 @@
-mkdir build64 & pushd build64
-cmake -DLUA_VERSION=5.3.5 -DLUAC_COMPATIBLE_FORMAT=ON -G "Visual Studio 16 2019" -A x64  ..
-popd
-cmake --build build64 --config Release
-md plugin_lua53\Plugins\x86_64
-copy /Y build64\Release\xlua.dll plugin_lua53\Plugins\x86_64\xlua.dll
-
-pause
