6122ddebfb1fb3ad81a6ca83e4b9b555208d73f8
蒋佳李 <jiali.jiang@ustargames.com>
2023-04-23 12:56:58 +0800

    优化编译

--- make_ios_lua53.sh
+++ make_ios_lua53.sh
@@ -1,6 +1,5 @@
 mkdir -p build_ios && cd build_ios
-cmake -DCMAKE_TOOLCHAIN_FILE=cmake/ios.toolchain.cmake -DPLATFORM=OS64 -DLUA_VERSION=5.3.5 -DLUAC_COMPATIBLE_FORMAT=ON -DCMAKE_BUILD_TYPE=Release -GXcode ../
-cd ..
+cmake -DPLATFORM=OS64 -DLUA_VERSION=5.3.5 -DLUAC_COMPATIBLE_FORMAT=ON -DCMAKE_BUILD_TYPE=Release
 cmake --build build_ios --config Release
 mkdir -p plugin_lua53/Plugins/iOS/
 cp build_ios/Release-iphoneos/libxlua.a plugin_lua53/Plugins/iOS/libxlua.a 
--- make_osx_silicon_lua53.sh
+++ make_osx_silicon_lua53.sh
@@ -1,6 +1,5 @@
 mkdir -p build_osx_silicon && cd build_osx_silicon
-cmake -DBUILD_SILICON=ON -DLUA_VERSION=5.3.5 -DLUAC_COMPATIBLE_FORMAT=ON -DCMAKE_BUILD_TYPE=Release -GXcode ../
-cd ..
+cmake -DBUILD_SILICON=ON -DLUA_VERSION=5.3.5 -DLUAC_COMPATIBLE_FORMAT=ON -DCMAKE_BUILD_TYPE=Release
 cmake --build build_osx_silicon --config Release
 mkdir -p plugin_lua53/Plugins/arm64/
 cp build_osx_silicon/Release/libxlua.dylib plugin_lua53/Plugins/arm64/
